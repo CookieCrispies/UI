4a94414388785574f7cd1bb2d9a668c6165006de
fcemmon <85023510+carlatic-dev@users.noreply.github.com>
2023-08-28 13:49:59 -0700

    Update fetchFarmsPrices.ts

--- src/state/farms/fetchFarmsPrices.ts
+++ src/state/farms/fetchFarmsPrices.ts
@@ -61,7 +61,7 @@ export const getFarmBaseTokenPrice = (farm: Farm, quoteTokenFarm: Farm, bnbPrice
 
 export const getFarmQuoteTokenPrice = (farm: Farm, quoteTokenFarm: Farm, bnbPriceBusd: BigNumber): BigNumber => {
   if (farm.quoteToken.symbol === 'WETH') {
-    return new BigNumber(1)
+    return new BigNumber(1644.32)
   }
 
   if (farm.quoteToken.symbol === 'WBONE') {
@@ -85,7 +85,7 @@ export const getFarmQuoteTokenPrice = (farm: Farm, quoteTokenFarm: Farm, bnbPric
 
 const fetchFarmsPrices = async (farms) => {
   const bnbBusdFarm = farms.find((farm: Farm) => farm.pid === 2)
-  const bnbPriceBusd = bnbBusdFarm.tokenPriceVsQuote ? new BigNumber(1).times(bnbBusdFarm.tokenPriceVsQuote) : BIG_ZERO
+  const bnbPriceBusd = bnbBusdFarm.tokenPriceVsQuote ? new BigNumber(1644.32).times(bnbBusdFarm.tokenPriceVsQuote) : BIG_ZERO
   // console.log('bnbPriceBusd',bnbPriceBusd.toFormat(10))
 
   const farmsWithPrices = farms.map((farm) => {
